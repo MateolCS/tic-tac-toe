5cb22c436d72408f74e2e475948a43dbcbbe6efd
MateoLCS <mateusz.namysl0318@gmail.com>
2022-01-14 23:44:09 +0100

    experimenting

--- script.js
+++ script.js
@@ -8,13 +8,7 @@ const Board = () =>{
 
     const getBoard = () => board
 
-    const updateBoard = (index, currentPlayer) =>{
-        if(board[index] != ''){
-            return
-        }else{
-            board[index] = currentPlayer.getSign()
-        }
-    } 
+    const updateBoard = (index, currentPlayer) => board[index] = currentPlayer.getSign() 
 
     const clearBoard = () =>{
         for(i=0; i<board.length; i++){
@@ -22,8 +16,39 @@ const Board = () =>{
         }
         return board
     }
+
+    const checkIfPossible = (index) =>{
+        if(board[index] != ''){
+            return true
+        }else{
+            return false
+        }
+    }
     
-    return {getBoard, updateBoard, clearBoard}
+    return {getBoard, updateBoard, clearBoard, checkIfPossible}
 }
+
+const game = (()=>{
+    let PlayerX = Player('X')
+    let PlayerO = Player('O')
+    let currentPlayer = PlayerX
+    let roundCount = 1
+    let gameBoard = Board()
+    let gameFields = document.querySelectorAll('.square')
+    console.log(gameFields)
+
+    while(roundCount <= 9){
+        gameFields.forEach(gameField =>{
+            gameField.addEventListener('click', ()=>{
+                let index = 0
+                if(gameBoard.checkIfPossible() == true){
+                    gameBoard.updateBoard(index, currentPlayer.getSign())
+                }else{
+                    return
+                }
+            })
+        })
+    }
+})
 
 
