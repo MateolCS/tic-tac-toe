5492057718fd314a2a0e625a775457cc418ba2d0
MateoLCS <mateusz.namysl0318@gmail.com>
2022-01-16 23:17:43 +0100

    final version

--- script.js
+++ script.js
@@ -17,34 +17,34 @@ const gameBoard = (() =>{
     }
 
     const checkForWinner = (currentPlayer) =>{
-        if(board[0] === currentPlayer.getSign()){
-            if(board[1] === currentPlayer.getSign() && board[2] === currentPlayer.getSign()){
+        if(board[0] === currentPlayer){
+            if(board[1] === currentPlayer && board[2] === currentPlayer){
                 return true
             }
-            if(board[3] === currentPlayer.getSign() && board[6] === currentPlayer.getSign()){
+            if(board[3] === currentPlayer && board[6] === currentPlayer){
                 return true
             }
-            if(board[4] === currentPlayer.getSign() && board[8] === currentPlayer.getSign()){
-                return true
-            }
-        }
-        if(board[2] === currentPlayer.getSign()){
-            if(board[5] === currentPlayer.getSign() && board[8] === currentPlayer.getSign()){
-                return true
-            }
-            if(board[4] === currentPlayer.getSign() && board[6] === currentPlayer.getSign()){
+            if(board[4] === currentPlayer && board[8] === currentPlayer){
                 return true
             }
         }
-        if(board[7] === currentPlayer.getSign()){
-            if(board[4] === currentPlayer.getSign() && board[1] === currentPlayer.getSign()){
+        if(board[2] === currentPlayer){
+            if(board[5] === currentPlayer && board[8] === currentPlayer){
                 return true
             }
-            if(board[6] === currentPlayer.getSign() && board[8] === currentPlayer.getSign()){
+            if(board[4] === currentPlayer && board[6] === currentPlayer){
                 return true
             }
         }
-        if(board[3] === currentPlayer.getSign() && board[4] === currentPlayer.getSign() && board[5] === currentPlayer.getSign()){
+        if(board[7] === currentPlayer){
+            if(board[4] === currentPlayer && board[1] === currentPlayer){
+                return true
+            }
+            if(board[6] === currentPlayer && board[8] === currentPlayer){
+                return true
+            }
+        }
+        if(board[3] === currentPlayer && board[4] === currentPlayer && board[5] === currentPlayer){
             return true
         }
 
@@ -57,15 +57,41 @@ const gameBoard = (() =>{
         }
     }
 
-    return{clearBoard, updateBoard, checkForWinner}
+    const getField = (index) =>{
+        return board[index]
+    }
+
+    return{clearBoard, updateBoard, checkForWinner, getField}
 })()
 
 
 const displayController = (() =>{
-    let gameFields = document.querySelectorAll('.square')
+    let gameFields = Array.from(document.getElementsByClassName('square'))
     let resetButton = document.getElementById('reset-button')
     let messageField = document.querySelector('.communication-output-text')
 
+    gameFields.forEach((gameField) =>{
+        gameField.addEventListener('click', (e) =>{
+            if(isEmpty(e)){
+                gameController.playRound(parseInt(e.target.dataset.index))
+                updateDisplayedBoard()
+            }
+        })
+    })
+
+
+    resetButton.addEventListener('click', () =>{
+        sendMsg('PlayerX turn')
+        gameBoard.clearBoard()
+        gameController.newGame()
+        updateDisplayedBoard()
+    })
+
+    const updateDisplayedBoard = () =>{
+        for(i = 0; i < gameFields.length; i++){
+            gameFields[i].textContent = gameBoard.getField(i)
+        }
+    }
 
     const sendMsg = (message) =>{
         messageField.textContent = message
@@ -79,6 +105,9 @@ const displayController = (() =>{
         }
     }
 
+    const isEmpty = (value) =>{
+        return value.target.textContent !== '' ? false : true
+    }
 
     return{sendMsg, setResultMsg}
 
@@ -86,6 +115,37 @@ const displayController = (() =>{
 
 const gameController = (() =>{
 
+    const playerX = Player('X')
+    const playerO = Player('O')
+    let round = 1
+
+
+    const playRound = (fieldIndex) =>{
+        gameBoard.updateBoard(fieldIndex, getCurrentPlayer())
+        if(gameBoard.checkForWinner(getCurrentPlayer())){
+            displayController.setResultMsg(getCurrentPlayer())
+            return
+        }
+        if(round === 9){
+            displayController.setResultMsg('Draw')
+            return
+        }
+        round ++
+        displayController.sendMsg(`${getCurrentPlayer()} turn!`)
+    }
+
+    const getCurrentPlayer = () =>{
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign()
+    }
+
+    const newGame = () =>{
+        round = 1
+    }
+
+    return {newGame, playRound}
 })()
+
+
+
 
 
